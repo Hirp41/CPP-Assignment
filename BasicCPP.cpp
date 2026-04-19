700141068b80ab851f248d53e37cca1a0757837e
Hirp41 <phir989@gmail.com>
2026-04-19 16:09:08 +0530

    CPP commit

---
+++ BasicCPP.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string name;
+    int age;
+    cout<<"Hello World";
+    cout<<"\nYour name: ";
+    cin>>name ;
+    cout<<"\nYour age: ";
+    cin>> age;
+
+    cout<<"\nHello "<<name<<"\nYour age is "<<age<<" nice to meet you";
+}
---
+++ CPPDemo.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Rectangle {
+private:
+    float length, width;
+public:
+    void getData() {
+        cout << "Enter length: ";
+        cin >> length;
+        cout << "Enter width: ";
+        cin >> width;
+    }
+    float calculateArea() {
+        return length * width;
+    }
+};
+int main() {
+    Rectangle r;   // Object of class
+    r.getData();   // Input values
+    cout << "Area of Rectangle = " << r.calculateArea() << endl;
+    return 0;
+}
